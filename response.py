a63a4513d35bb1ceb2852adf694ea1ff389b2738
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2022-06-16 15:42:07 -0400

    Add files via upload

--- response.py
+++ response.py
@@ -1,19 +1,44 @@
 import urllib.request
 import urllib.error
-def read_file(filename,ssl_context):
-    '''Read file line by line and calls the function to check response'''
-    with open(filename , 'r') as file:
-        for line in file:
-            check_response(line,ssl_context)
+import sys
+import asyncio
 
-def check_response(url,ssl_context):
-    '''Check the response of url and returns exception type if error'''
-    try:
-        response_code=urllib.request.urlopen(url,context=ssl_context).getcode()
-    except urllib.error.URLError as e:
-        response_code=404
-    if response_code==200:
-        status="active"
+
+def get_url_from_file(filename):
+    """
+    Description: Read file line by line and return the urls
+    :param filename: the name of the file
+    """
+    urls = set()
+    with open(filename, 'r') as file:
+        for line in file:
+            urls.add(line)  #todo add url validation regex
+    return urls
+
+
+def get_urls(url_args):
+    """return a set of URLs"""
+    if url_args is not None:
+        urls = set()
+        try:
+            for i in url_args:
+                urls.add(i)
+            return urls
+        except ValueError:
+            print("Unexpected argument error")
     else:
-        status="inactive"
-    print("{}  {}".format(url,status))
+        print("Please specify at least one url")
+        sys.exit(3)
+
+
+async def check_response(url, ssl_context):
+    """Check the response of url and returns exception type if error"""
+    try:
+        response_code = urllib.request.urlopen(url, context=ssl_context).getcode()
+    except urllib.error.URLError as e:
+        response_code = 404
+    if response_code == 200:
+        status = "active"
+    else:
+        status = "inactive"
+    print("{}  {}".format(url, status))
--- test.py
+++ test.py
@@ -1,22 +1,41 @@
 import argparse
 import ssl
-from response import read_file
-from response import check_response
-parser = argparse.ArgumentParser()
-parser.add_argument("--infile", "-f", type=str, required=False)
-parser.add_argument("--url", "-u" , type=str,required=False)
-args = parser.parse_args()
-filename=args.infile
-url=args.url
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False             # python3.6+ don't work with ssl certifcates without any library so making ssl_cert as none.
-ssl_context.verify_mode = ssl.CERT_NONE
-if(url and filename):
-    check_response(url,ssl_context)
-    read_file(filename,ssl_context)
-elif(url):
-    check_response(url,ssl_context)
-elif(filename):
-    read_file(filename,ssl_context)
-else:
-    print("You haven't passed url or file")
+import asyncio
+from response import get_url_from_file, check_response, get_urls
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infile", "-f", type=str, required=False)
+    parser.add_argument("--url", "-u", nargs='*', required=False)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    filename = args.infile
+    url_args = args.url
+    ssl_context = ssl.create_default_context()
+    # python3.6+ don't work with ssl certifcates without any library so making ssl_cert as none.
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    urls = []
+    if url_args and filename:
+        urls = get_urls(url_args) + get_url_from_file(filename)
+    elif url_args:
+        urls = get_urls(url_args)
+    elif filename:
+        urls = get_url_from_file(filename)
+    else:
+        print("You haven't passed url or file")
+
+    # creating tasks for asynchronous execution
+    tasks = []
+    for url in urls:
+        tasks.append(asyncio.Task(check_response(url, ssl_context)))
+    yield from asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
